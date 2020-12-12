f505bba130afea37314c87b6f24876d35a2c1757
yonghwa <yonghwa9086@naver.com>
2020-12-12 09:58:13 +0900

    TODO: solve 16916

--- 16916.java
+++ 16916.java
@@ -4,31 +4,56 @@ import java.io.InputStreamReader;
 
 public class Main {
     private static String S, P;
+    private static int[] failFunc;
     public static void main(String[] args) {
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             S = br.readLine();
             P = br.readLine();
 
+            failFunc = new int[P.length()];
+
+            makeFailFunc();
+
             for (int i = 0; i + P.length() <= S.length(); i++) {
-                boolean isPartString = true;
-                for (int j = 0; j < P.length(); j++) {
-                    if (S.charAt(i + j) != P.charAt(j)) {
-                        isPartString = false;
-                        break;
+                for (int j = 0; j < S.length(); j++) {
+                    if (S.charAt(i + j) == P.charAt(j)) {
+                        j++;
+                        if (j >= P.length()) {
+                            System.out.println(1);
+                            return;
+                        }
+                    } else {
+                        i = j - failFunc[j - 1];
+                        j = failFunc[j - 1];
                     }
                 }
-
-                if (isPartString) {
-                    System.out.println(1);
-                    return;
-                }
             }
+
             System.out.println(0);
         }
         catch (IOException e){
             e.printStackTrace();
             System.out.println(e.getMessage());
+        }
+    }
+
+    private static void makeFailFunc () {
+        for (int i = 1, j = 0; i < S.length(); i++) {
+            if (S.charAt(i + j) == P.charAt(j)) {
+                j++;
+            } else {
+                j--;
+            }
+
+            if (j >= i) {
+                failFunc[i] = j + 1;
+                i++;
+            } else if (j < 0) {
+                failFunc[i] = 0;
+                j = 0;
+                i++;
+            }
         }
     }
 }
